d613103ecffce6b237390b67325106b87de4280f
HKmus <mus.kch03@gmail.com>
2025-04-06 05:08:44 +0100

    fixed the callback error and the removed unused packages

--- app/(auth)/auth/callback/route.ts
+++ app/(auth)/auth/callback/route.ts
@@ -7,6 +7,7 @@ export async function GET(request: Request) {
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
+
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
@@ -23,6 +24,7 @@ export async function GET(request: Request) {
       }
     }
   }
+
   // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
